3256fffe34155b1f232679601f6e508d552b5a53
piedcipher <pateltirth454@gmail.com>
2019-10-05 12:53:42 +0530

    Fixed FileSystemException, Added types for all things

--- bin/main.dart
+++ bin/main.dart
@@ -3,38 +3,43 @@ import 'dart:io';
 
 import 'package:http/http.dart' as http;
 
-void main(List<String> arguments) async {
-  final username = getInput(arguments);
-  final followers = await getAllFollowers(username);
-  final file = File(Directory.current.path + '/followers.txt');
-  final oldFollowers = await file
+void main(final List<String> arguments) async {
+  final String username = getInput(arguments);
+  final List<String> followers = await getAllFollowers(username);
+  final File file = File('followers.txt');
+  if (!file.existsSync()) {
+    file.writeAsStringSync('');
+  }
+
+  final List<String> oldFollowers = await file
       .openRead()
       .transform(utf8.decoder)
-      .transform(new LineSplitter())
+      .transform(LineSplitter())
       .toList();
-  final followerOwner = oldFollowers.first;
+  final String followerOwner =
+      oldFollowers.isNotEmpty ? oldFollowers.first : '';
   if (followerOwner != username) {
     writeFollowers(followers, file, username);
-    print("We have recorded your followers. Rats will be shown next time you "
-        "run the program.");
+    print(
+        'We have recorded your followers. Rats will be shown next time you run the program.');
     return;
   }
 
-  final rats = await Stream.fromIterable(oldFollowers)
+  final List<String> rats = await Stream.fromIterable(oldFollowers)
       .skip(1)
       .where((f) => !followers.contains(f))
       .toList();
   if (rats.isNotEmpty) {
-    print("These are your rats\n");
+    print('These are your rats\n');
     rats.forEach((f) => print(f));
   } else {
-    print("Impressive! All your followers are loyal.");
+    print('Impressive! All your followers are loyal.');
   }
 
   writeFollowers(followers, file, username);
 }
 
-String getInput(List<String> arguments) {
+String getInput(final List<String> arguments) {
   if (arguments.isEmpty) {
     stdout.write('Please enter a username: ');
     return stdin.readLineSync(retainNewlines: false);
@@ -44,22 +49,22 @@ String getInput(List<String> arguments) {
 }
 
 void writeFollowers(List<String> followers, File sink, String owner) {
-  //Adding username so we can figure out whose followers are these
+  // Adding username so we can figure out whose followers are these
   followers.insert(0, owner);
   sink.openWrite(mode: FileMode.write).writeAll(followers, "\n");
 }
 
 Future<List<String>> getAllFollowers(String username, {int pageNo = 1}) async {
-  final apiURL =
+  final String apiURL =
       'https://api.github.com/users/$username/followers?per_page=100&'
       'page=$pageNo';
-  final response = await http.get(apiURL);
-  final jsonResponse = jsonDecode(response.body);
+  final http.Response response = await http.get(apiURL);
+  final List<dynamic> jsonResponse = jsonDecode(response.body);
   if (jsonResponse is List && jsonResponse.isEmpty) {
-    return new List<String>();
+    return List<String>();
   } else {
-    var users = await getAllFollowers(username, pageNo: pageNo + 1);
-    users.addAll((jsonResponse as List).map((i) => i['login']));
+    List<String> users = await getAllFollowers(username, pageNo: pageNo + 1);
+    users.addAll((jsonResponse).map((i) => i['login']));
     return users;
   }
 }
